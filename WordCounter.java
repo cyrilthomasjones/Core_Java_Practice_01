d6862ca8047476eac19e4c54e94edd8ffb21ddd2
cyrilthomasjones <44472020+cyrilthomasjones@users.noreply.github.com>
2024-03-29 10:44:27 -0500

    Add files via upload

--- WordCounter.java
+++ WordCounter.java
@@ -1,4 +1,4 @@
-package programming_practice;
+package strings;
 
 import java.util.Scanner;
 
@@ -6,17 +6,20 @@ public class WordCounter {
 
 	public static void main(String[] args) {
 		System.out.println("Enter a String");
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();//Use nextLine() instead of next() to consider anything after first space character
-		int count = 0; 
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
 		
-		for (int i = 0; i < input.length(); i++) {
-			if(Character.isWhitespace(input.charAt(i))){
-				count++;
+		System.out.println(input.split("").length); // second one line solution 
+		
+		int counter = 0;
+		
+		for(int i=0; i< input.length(); i++ ) {
+			if(Character.isWhitespace(input.charAt(i))) {
+				counter++;
 			}
 		}
-		System.out.println(count+1); // We include (count+1) this to include the last space or word 
-		sc.close();
+		
+		System.out.println(counter + 1);
 	}
 
 }
