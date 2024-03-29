36286b15bf143649557b17789739b85c11caf140
cyrilthomasjones <44472020+cyrilthomasjones@users.noreply.github.com>
2024-03-29 10:44:09 -0500

    Add files via upload

--- StringReverser.java
+++ StringReverser.java
@@ -1,27 +1,21 @@
-package programming_practice;
+package strings;
 
 import java.util.Scanner;
 
 public class StringReverser {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Enter a String");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.next();
 		String output = "";
 		
-		for(int i=input.length() - 1; i >= 0; i--){
+		for (int i = input.length() - 1 ; i >= 0; i--) {
 			output += input.charAt(i);
 		}
 		System.out.println(output);
 		
-		//Using in built class or function to reverse a string.
 		System.out.println(new StringBuffer(input).reverse());
-		
-		
-		scanner.close();
-
 	}
 
 }
