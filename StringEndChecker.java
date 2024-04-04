07d20122ef429a361e3a3392a0decc4d8e871b05
cyrilthomasjones <44472020+cyrilthomasjones@users.noreply.github.com>
2024-04-03 20:03:31 -0500

    Add files via upload

--- StringEndChecker.java
+++ StringEndChecker.java
@@ -2,29 +2,40 @@ package strings;
 
 import java.util.Scanner;
 
+import javax.print.DocFlavor.INPUT_STREAM;
+
 public class StringEndChecker {
 
 	public static void main(String[] args) {
+		// Take Character of first String starting from ending 
+		
+		//Compare first string character in second string
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first string");
+		String firstString = sc.nextLine();
+		System.out.println("Enter second string");
+		String secondString = sc.nextLine();
+        System.err.println(firstString.endsWith(secondString) ? "String ends with second string" : "String does not end with second string");
+		
+		sc.close();
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter First String");
-		String s1 = scanner.nextLine();
-		System.out.println("Enter Second String");
-		String s2 = scanner.nextLine();
-		int count = 0;
-
-		for (int i = s1.length() - 1, j = s2.length() - 1; j >=0; i--, j--) {
-			if (s1.charAt(i) == s2.charAt(j)) {
-				count++;
-			}
-		}
-
-		if (count == s2.length()) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
-		}
-		scanner.close();
+		/*
+		 * int counter = 0;
+		 * 
+		 * // First Solution for (int i = firstString.length()-1, j =
+		 * secondString.length()-1; j >= 0; i--,j--) { if(firstString.charAt(i) !=
+		 * secondString.charAt(j)) { break; } counter++; }
+		 * 
+		 * if(counter == secondString.length()) {
+		 * System.out.println("String ends with second string"); }else {
+		 * System.out.println("String does not end with second string"); }
+		 * 
+		 * // Second Solution System.err.println(firstString.endsWith(secondString) ?
+		 * "String ends with second string" : "String does not end with second string");
+		 * 
+		 * sc.close();
+		 */
 	}
 
 }
