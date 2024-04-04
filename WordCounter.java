0f3cfed84c3ae444b1393e16daa641af5376d2a4
cyrilthomasjones <44472020+cyrilthomasjones@users.noreply.github.com>
2024-04-03 20:04:24 -0500

    Add files via upload

--- WordCounter.java
+++ WordCounter.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class WordCounter {
 
 	public static void main(String[] args) {
-		System.out.println("Enter a String");
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.nextLine();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		String input = sc.nextLine();
+		int count = 0;
+
+		//First solution using the split function 
+		System.out.println(input.split(" ").length);
+
+		/* Another solution is using the Character class
+		 This class has isWhitespace method */
 		
-		System.out.println(input.split("").length); // second one line solution 
-		
-		int counter = 0;
-		
-		for(int i=0; i< input.length(); i++ ) {
-			if(Character.isWhitespace(input.charAt(i))) {
-				counter++;
+		for (int i = 0; i < input.length(); i++) {
+			if (Character.isWhitespace(input.charAt(i))) {
+				count++;
 			}
 		}
-		
-		System.out.println(counter + 1);
+		System.out.println(count + 1);
+
 	}
 
 }
